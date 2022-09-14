9c0b31f1d0409df1637021287b897dd559744e0b
Ankit0827 <a.verma0827@gmail.com>
2022-09-14 22:45:06 +0530

    some changes in js

--- table.js
+++ table.js
@@ -1,119 +1,14 @@
-// const table= document.getElementById("table-color-change");
-// table.addEventListener("click", changecolor);
-var check1= false;
-function changecolor1() {
-  let x = document.getElementById("td-color1").innerHTML;
-  check1 = !check1;
-  console.log(x);
-  if (check1) {
-    document.getElementById("td-color1").style.color = "white";
-    document.getElementById("td-color1").style.backgroundColor = "green";
-  } else {
-    document.getElementById("td-color1").style.color = "black";
-    document.getElementById("td-color1").style.backgroundColor = "white";
-  }
-}
-var check2=false;
-function changecolor2() {
-  check2 = !check2;
-  let y = document.getElementById("td-color2").innerHTML;
-  console.log(y);
-  if (check2) {
-    document.getElementById("td-color2").style.color = "white";
-    document.getElementById("td-color2").style.backgroundColor = "green";
-  } else {
-    document.getElementById("td-color2").style.color = "black";
-    document.getElementById("td-color2").style.backgroundColor = "white";
-  }
-}
-var check3=false;
-function changecolor3() {
-    check3 = !check3;
-  let a = document.getElementById("td-color3").innerHTML;
-  console.log(a);
-  if (check3) {
-    document.getElementById("td-color3").style.color = "white";
-    document.getElementById("td-color3").style.backgroundColor = "green";
-  } else {
-    document.getElementById("td-color3").style.color = "black";
-    document.getElementById("td-color3").style.backgroundColor = "white";
-  }
-}
-var check4=false;
-function changecolor4() {
-    check4 = !check4;
-  let a = document.getElementById("td-color4").innerHTML;
-  console.log(a);
-  if (check4) {
-    document.getElementById("td-color4").style.color = "white";
-    document.getElementById("td-color4").style.backgroundColor = "green";
-  } else {
-    document.getElementById("td-color4").style.color = "black";
-    document.getElementById("td-color4").style.backgroundColor = "white";
-  }
-}
-var check5=false;
-function changecolor5() {
-    check5 = !check5;
-  let a = document.getElementById("td-color5").innerHTML;
-  console.log(a);
-  if (check5) {
-    document.getElementById("td-color5").style.color = "white";
-    document.getElementById("td-color5").style.backgroundColor = "green";
-  } else {
-    document.getElementById("td-color5").style.color = "black";
-    document.getElementById("td-color5").style.backgroundColor = "white";
-  }
-}
-var check6=false;
-function changecolor6() {
-    check6 = !check6;
-  let a = document.getElementById("td-color6").innerHTML;
-  console.log(a);
-  if (check6) {
-    document.getElementById("td-color6").style.color = "white";
-    document.getElementById("td-color6").style.backgroundColor = "green";
-  } else {
-    document.getElementById("td-color6").style.color = "black";
-    document.getElementById("td-color6").style.backgroundColor = "white";
-  }
-}
-var check7=false;
-function changecolor7() {
-    check7 = !check7;
-  let a = document.getElementById("td-color7").innerHTML;
-  console.log(a);
-  if (check7) {
-    document.getElementById("td-color7").style.color = "white";
-    document.getElementById("td-color7").style.backgroundColor = "green";
-  } else {
-    document.getElementById("td-color7").style.color = "black";
-    document.getElementById("td-color7").style.backgroundColor = "white";
-  }
-}
-var check8=false;
-function changecolor8() {
-    check8 = !check8;
-  let a = document.getElementById("td-color8").innerHTML;
-  console.log(a);
-  if (check8) {
-    document.getElementById("td-color8").style.color = "white";
-    document.getElementById("td-color8").style.backgroundColor = "green";
-  } else {
-    document.getElementById("td-color8").style.color = "black";
-    document.getElementById("td-color8").style.backgroundColor = "white";
-  }
-}
-var check9=false;
-function changecolor9() {
-    check9 = !check9;
-  let a = document.getElementById("td-color9").innerHTML;
-  console.log(a);
-  if (check9) {
-    document.getElementById("td-color9").style.color = "white";
-    document.getElementById("td-color9").style.backgroundColor = "green";
-  } else {
-    document.getElementById("td-color9").style.color = "black";
-    document.getElementById("td-color9").style.backgroundColor = "white";
-  }
+
+var check=false;
+function changecolor(e){
+  check=!check
+   let x=e.id;
+   if(check){
+    document.getElementById(x).style.color="white"
+    document.getElementById(x).style.backgroundColor="green"
+   }else{
+    document.getElementById(x).style.color="black"
+    document.getElementById(x).style.backgroundColor="white"
+   }
+ 
 }
